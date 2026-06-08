3e88d0df2dbb0acbc6a48e7bc6bad054aa3c6973
Karl Weinmeister <kweinmeister@google.com>
2026-06-08 10:31:58 -0500

    feat: add Terraform modules for GPU stress testing and Parallelstore CSI, plus associated benchmark tasks

---
+++ tf/prebuilt/parallelstore-csi/variables.tf
@@ -0,0 +1,31 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+  type        = string
+}
+
+variable "location" {
+  description = "GCP location (region or zone)"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "zone" {
+  description = "GCP zone for the cluster nodes and Parallelstore instance"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "node_count" {
+  type    = number
+  default = 1
+}
+
+variable "machine_type" {
+  type    = string
+  default = "n1-standard-4"
+}
